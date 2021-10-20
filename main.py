ceb946905ce6ee7cb56d68192fcea75a63143a51
Anton Iasmina <antonyasmina@yahoo.ro>
2021-10-20 11:22:41 +0300

    verificare1

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-def citirelista() -> list[int]:
-    lst = []
-    n = int(input("Dati un numar de elemente:"))
-    for i in range(n):
-        lst.append(int(input("l["+str(i)+"]=")))
-    return lst
 
-
-if __name__ == "__main__":
-
-    while True:
-        print("1.Citire date")
-        print("2. :")
-        print("3. :")
-        print("4. :")
-        print("5.")
-
-        optiune = input("Alege optiunea:")
-
-        if optiune =='1' :
-            sir=citirelista()
-        elif optiune=='2':
-            break
+if __name__=="__main__":
+    print("da")
